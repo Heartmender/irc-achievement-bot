f97cdc580b5b55072a9715353b5aa9e261992dd8
Jade Rain <jaderain32@gmail.com>
2013-06-29 02:53:23 -0500

    Fixed issue where Actor would send award through /me and not MSG

--- src/achievements/Actor.java
+++ src/achievements/Actor.java
@@ -21,7 +21,7 @@ public class Actor extends Achievement {
 		if(db.getCount(nick, getAchievementId()) >= 10) {
 			if(!db.hasAchievement(nick, getAchievementId())) {
 				db.giveAchievement(nick, getAchievementId());
-				event.respond(getAwardString(nick));
+				event.getBot().sendMessage("#tulpa", getAwardString(nick));
 			}
 		}
 	}
